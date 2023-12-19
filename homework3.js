0ae7741b24661e2478c4c9d1bff5274fa89a79e0
Veronika Puhach <onika.punik@gmail.com>
2023-12-19 22:35:48 +0300

    Corrected homework3

--- homework3.js
+++ homework3.js
@@ -1,12 +1,12 @@
-let year = prompt("Введите год получения");
+let year = prompt("Введите Ваш возраст");
 
 if (!isNaN(year) && year > 0) {
   if (year % 10 === 1 && year % 100 !== 11) {
-    console.log('${year} год');
+    console.log(`${year} год`);
   } else if (year % 10 >= 2 && year % 10 <= 4 && (year % 100 < 10 || year % 100 >= 20)) {
-    console.log('${year} года');
+    console.log(`${year} года`);
   } else {
-    console.log('${year} лет');
+    console.log(`${year} лет`);
   }
 } else {
   console.log("Вы ввели некорректное значение");

596d3a7d81f9cb0acdccce212873219211df288f
Veronika Puhach <onika.punik@gmail.com>
2024-01-09 01:06:17 +0300

    Homework7

--- homework7.js
+++ homework7.js
@@ -1,58 +1,64 @@
 //ДЗ-1
 let PS5 = {
-    model: "PlayStation 5",
-    storageCapacity: "1TB",
-    color: "White",
+  model: "PlayStation 5",
+  storageCapacity: "1TB",
+  color: "White",
 
-    getInfo: function() {
-      for (let key in this) {
-        if (typeof this[key] !== 'function') {
-          console.log(key + ": " + this[key]);
-        }
+  getInfo: function() {
+    for (let key in this) {
+      if (typeof this[key] !== 'function') {
+        console.log(key + ": " + this[key]);
       }
     }
-  };
-  PS5.getInfo(); 
-  PS5.newProperty = ' Price - 499$'; 
-  PS5.getInfo(); 
+  }
+};
+PS5.getInfo(); 
+PS5.newProperty = ' Price - 499$'; 
+PS5.getInfo(); 
 
 //ДЗ-2
 var services = {
-    "Маникюр": 250,
-    "Маникюр с покрытием Luxio": 450,
-    "Педикюр с покрытием Luxio": 500
-  };
-  services.price = function() {
-    var total = 0;
-    for (var service in this) {
-      if (typeof this[service] === 'number') {
-        total += this[service];
-      }
-    }
-    return total;
-  };
+"стрижка": "60 грн",
+"бритье": "80 грн",
+"Мойка головы": "100 грн"
+};
 
-  services.minPrice = function() {
-    var min = Infinity;
-    for (var service in this) {
-      if (typeof this[service] === 'number' && this[service] < min) {
-        min = this[service];
-      }
-    }
-    return min;
-  };
-  
-  services.maxPrice = function() {
-    var max = 0;
-    for (var service in this) {
-      if (typeof this[service] === 'number' && this[service] > max) {
-        max = this[service];
-      }
-    }
-    return max;
-  };
-  console.log(services.price()); 
-  console.log(services.minPrice()); 
-  console.log(services.maxPrice()); 
+services['Разбить стекло'] = "200 грн";
 
-  services["Мужской маникюр"] = 300;
+services.price = function() {
+var total = 0;
+for (var service in this) {
+    if (typeof this[service] === 'string') {
+        total += parseInt(this[service]);
+    }
+}
+return total + " грн";
+};
+services.minPrice = function() {
+var min = Infinity;
+for (var service in this) {
+    if (typeof this[service] === 'string') {
+        var price = parseInt(this[service]);
+        if (price < min) {
+            min = price;
+        }
+    }
+}
+return min + " грн";
+};
+services.maxPrice = function() {
+var max = 0;
+for (var service in this) {
+    if (typeof this[service] === 'string') {
+        var price = parseInt(this[service]);
+        if (price > max) {
+            max = price;
+        }
+    }
+}
+return max + " грн";
+};
+
+console.log(services.price()); 
+console.log(services.minPrice()); 
+console.log(services.maxPrice()); 
